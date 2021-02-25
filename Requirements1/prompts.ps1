a25a15d46ec107a9227e89b430b8adafa5bacdaf
scottifydev <scott@scottify.io>
2021-02-25 14:47:31 -0500

    Finished switch statment and comments

--- Requirements1/prompts.ps1
+++ Requirements1/prompts.ps1
@@ -1,26 +1,75 @@
 # Scott Lee - 001397458
 
-# A.  Create a PowerShell script named “prompts.ps1” within the “Requirements1” folder. For the first line, create a comment and include your first and last name along with your student ID.
+# Clearing screen for a nice application look
+Clear-Host
 
-# Note: The remainder of this task should be completed within the same script file, prompts.ps1.
+# Creating prompt text in a variable
+$prompt = "EEP1 TASK 1: SCRIPTING IN POWERSHELL`n`rEnter a number 1-5 to Select From the Following Tasks`n`r1: Update DailyLog.txt`n`r2: Update C916contents.txt`n`r3: View Proccessor Time & Memory Usage`n`r4: View System Processes`n`r5: Exit`n`r"
 
-# B.  Create a “switch” statement that continues to prompt a user by doing each of the following activities, until a user presses key 5:
+# A function to rewrite the screen after user input. Takes Variable from the switch since it is outside of the scope of the switch input variable
+function Run-Task ($TaskNumber) {
+    Write-Host "Running Task $TaskNumber"
+    Start-Sleep -Seconds 1.5
+    Clear-Host
+    Write-Host "Task $TaskNumber Complete!`n`r"
+}
 
+# Do-Until loop to cycle through user inputs
 do {
     
-    $input = Read-Host -Prompt "Input"
+    # Switch with user input for task number
+    $input = Read-Host $prompt 
     switch ($input) {
         
-        # 1.   Using a regular expression, list files within the Requirements1 folder, with the .log file extension and redirect the results to a new file called “DailyLog.txt” within the same directory without overwriting existing data. Each time the user selects this prompt, the current date should precede the listing. (User presses key 1.)
-        1 {Add-Content -Path .\DailyLog.txt -Value "Log files for: $(Get-Date)`n`r$(Get-ChildItem *.log | Format-Table -AutoSize | Out-String)"}
-        # 2.  List the files inside the “Requirements1” folder in tabular format, sorted in ascending alphabetical order. Direct the output into a new file called “C916contents.txt” found in your “Requirements1” folder. (User presses key 2.)
-        2 {Set-Content -Path .\C916contents.txt -Value (Get-ChildItem .\* | Format-Table -AutoSize | Out-String)}
-        # 3. Use counters to list the current CPU % Processor Time and physical memory usage. Collect 4 samples with each sample being 5 seconds intervals. (User presses key 3.) 
-        3 {type .\C916contents.txt}
-        # 4{ }
-        Default {"Nothing!"}
+        # 1. On user input of "1", Add-Content cmdlet add a String with the current date using Get-Date and all of the .log files in the current directory using Get-ChildItem and formatted into a readable table, and outputted as a string. New contents are added to the head of the DailyLog.txt without destroying the existing contents. The Run-Task function is executed and passes along to input variable.
+        
+        1 {
+            Add-Content -Path .\DailyLog.txt -Value "Log files for: $(Get-Date)`n`r$(Get-ChildItem *.log | Format-Table -AutoSize | Out-String)"
+            $(Run-Task $input)
+        }
+        
+        # 2. On user input of "2", Set-content creates or overwrites the contents fo C916contents.txt using the Get-Child cmdlet. The contents are formatted into a readable table with Format-Table. The Run-Task function is executed and passes along to input variable.
+        
+        2 {
+            Set-Content -Path .\C916contents.txt -Value (Get-ChildItem .\* | Format-Table -AutoSize | Out-String)
+            $(Run-Task $input)
+        }
+        
+        # On user input of "3", the screen is cleared and the user is notified that the script is measuring system performance. The Get-Counter cmdlet measures 4 samples of the Processor Time and Commited Memory at 5 second intervals and displays them with a prompt for the user to continue. The screen is cleared and the main prompt brought back up. 
+        
+        3 {
+            Clear-Host
+            Write-Host 'Measuring System Performance...'
+            Get-Counter -Counter "\Processor(_Total)\% Processor Time", "\Memory\Committed Bytes" -SampleInterval 5 -MaxSamples 4
+            read-host 'Press Enter to continue'
+            Clear-Host
+            
+        }
+        
+        # 4.  On user input of "4", the powershell grid view window is opened displaying all running processes sorted by CPU time in decending order. The script waits for the window to be closed before proceeding. Screen is cleared and user is reprompted for input.
+        
+        4 {
+            Get-Process | Sort-Object CPU -desc | Out-GridView -Wait
+            Clear-Host
+        }
+
+        # 5.  On user input of "5", the screen is cleared and the user is notified that the script is terminating. The Do until loop is fulfilled and the program exits.
+        
+        5 {
+            Clear-Host 
+            Write-Host "Exiting..."
+        }
+
+        # On any input that doesn't match any of the switch cases, the user is prompted to make a valid entry.
+        
+        Default {
+            Clear-Host
+            Write-Host "Please Make a Valid Entry" -ForegroundColor red
+        }
     }
 } 
+
+# The until clause of Do loop is defined when the user inputs "5" to close end the script.
 
 until ($input -eq '5')
 
@@ -29,9 +78,7 @@ until ($input -eq '5')
 
 
 
-# 4.  List all the different running processes inside your system. Sort the output by processor time in seconds greatest to least, and display it in grid format. (User presses key 4.) 
 
-# 5.  Exit the script execution. (User presses key 5.)
 
  
 
