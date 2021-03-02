ef3d630448d67a9955d6d2f2dd407f78143b0e4e
scottifydev <scott@scottify.io>
2021-03-02 10:52:03 -0500

    Create For-Each Loop to add entries new OU

---
+++ Requirements2/restore.ps1
@@ -0,0 +1,78 @@
+ 
+
+# B.  Write a single script within the “restore.ps1” file that performs all of the following functions without user interaction:
+
+# New-ADOrganizationalUnit -Name finance -ProtectedFromAccidentalDeletion $false
+
+$NewAD = Import-Csv $PSScriptRoot\financePersonnel.csv
+$Path = "ou=finance,DC=ucertify,DC=com"
+
+foreach ($ADUser in $NewAD) {
+
+    $First = $ADUser.First_Name
+    $Last = $ADUser.Last_Name
+    $Name = $ADUser.First_Name, $ADUser.Last_Name -join ' ' 
+    $Postal = $ADUser.PostalCode
+    $Office = $ADUser.OfficePhone
+    $Mobile = $ADUser.MobilePhone
+
+    # New-AdUser -Name $Name -GivenName $First -Surname $Last -PostalCode $Postal -OfficePhone $Office -MobilePhone $Mobile -Path $Path
+    
+    $String = @" 
+    $First
+    $Last
+    $Name
+    $Postal
+    $Office
+    $Mobile
+"@
+    Write-Host  $String
+    $Last
+    $Name
+    $Postal
+    $Office
+    $Mobile
+}
+
+
+
+# 1.  Create an Active Directory organizational unit (OU) named “finance.”
+
+# 2.  Import the financePersonnel.csv file (found in the “Requirements2” directory) into your Active Directory domain and directly into the finance OU. Be sure to include the following properties:
+
+# •  First Name
+
+# •  Last Name
+
+# •  Display Name (First Name + Last Name, including a space between)
+
+# •  Postal Code
+
+# • Office Phone
+
+# • Mobile Phone
+
+
+# 3.  Create a new database on the UCERTIFY3 SQL server instance called “ClientDB.”
+
+# 4.  Create a new table and name it “Client_A_Contacts.” Add this table to your new database.
+
+# 5.  Insert the data from the attached “NewClientData.csv” file (found in the “Requirements2” folder) into the table created in part B4.
+ 
+
+# C.  Apply exception handling using try-catch for System.OutOfMemoryException.
+ 
+
+# D.    Run the script within the uCertify environment. After the script executes successfully, run the following cmdlets individually from within your Requirements2 directory:
+# 1. Get-ADUser -Filter * -SearchBase “ou=finance,dc=ucertify,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
+# 2. Invoke-Sqlcmd -Database ClientDB –ServerInstance .\UCERTIFY3 -Query ‘SELECT * FROM dbo.Client_A_Contacts’ > .\SqlResults.txt
+
+
+
+# Note: Ensure you have all of the following files intact within the “Requirements2” folder, including the original files:
+
+# •  “restore.ps1”
+
+# •  “AdResults.txt”
+
+# •  “SqlResults.txt”
